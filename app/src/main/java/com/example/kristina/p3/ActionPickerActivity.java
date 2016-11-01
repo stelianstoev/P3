dc55d6d9cd91685f5c62c6069ddba2923dd18b65
peter.r.w@live.de <sT33-fDjS5>
2016-11-01 23:58:18 +0100

    Activity Action Picker

---
+++ app/src/main/java/com/example/kristina/p3/ActionPickerActivity.java
@@ -0,0 +1,13 @@
+package com.example.kristina.p3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActionPickerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_action_picker);
+    }
+}
